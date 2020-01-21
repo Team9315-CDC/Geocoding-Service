e223ced5363c5afcee21560d673924126a9cc2d4
Jeff Zhan <jeffzhan2008@gmail.com>
2020-01-21 10:54:19 -0500

    some copy pasta javascript for "error checking" styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-document.getElementById("submit").addEventListener("click", () => alert("The form has been submitted"));;
+// document.getElementById("submit").addEventListener("click", () => alert("Test"));;
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
 
 let stateOptionsList = document.getElementById('inputState').options;
 let stateOptions = [{
