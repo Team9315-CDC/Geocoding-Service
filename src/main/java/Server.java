59a3727df6afb4c0561ad227c50bcb0cdb5fbe42
dallas-d <dallasd@gatech.edu>
2020-02-02 15:01:06 -0500

    Sample server and simple geocoder set up to get requests from the US Census geocoder

---
+++ src/main/java/Server.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import main.java.geocoder.*;
+
+public class Server {
+
+
+    public static void main(String[] args) {
+
+        Address sample = new Address("4600+Silver+Hill+Rd%2C+Suitland%2C+MD+20746");
+        GeocodeRequest gr = new GeocodeRequest(sample);
+        boolean status = false;
+        try {
+            status = gr.submitRequest();
+        } catch (Exception e) {
+            System.out.println("Unexpected exception!");
+        }
+        if (status) {
+            gr.printResponse();
+        } else {
+            System.out.println("Something went wrong?");
+        }
+    }
+
+}
