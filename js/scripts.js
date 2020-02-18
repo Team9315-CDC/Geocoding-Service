4173947384d235312a80742baa860d530f818991
Jeff Zhan <jeffzhan2008@gmail.com>
2020-02-18 11:03:18 -0500

    gotta love cors errors

--- js/scripts.js
+++ js/scripts.js
@@ -20,35 +20,44 @@ document.getElementById("batchImportLink").addEventListener("click", () => {
 
 document.getElementById("submit").addEventListener("click", () => {
     // fetch results from geocoder
-    fetch('https://geocoding.geo.census.gov/geocoder/geographies/address?street=4600+Silver+Hill+Rd&city=Suitland&state=MD&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json')
+    let jsonResponse;
+
+    fetch('https://geocoding.geo.census.gov/geocoder/locations/address?street=4600+Silver+Hill+Rd&city=Suitland&state=MD&zip=20746&benchmark=Public_AR_Census2010&format=json')
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
-            console.log(myJson);
+            // console.log(myJson);
+            jsonResponse = myJson;
+            //will need to do some checks here: 
+            x = myJson.result.addressMatches[0].coordinates.x
+            y = myJson.result.addressMatches[0].coordinates.y
+            let confirmation = "The address coordinates of (" + x.toString() + ", " + y.toString() + ") have been geocoded."
+            console.log(confirmation.toString())
+            console.log($('#modal').find(".modal-body"))
+            $('#modal').find(".modal-body").text(confirmation)
+            $('#modal').modal('show');
+        })
+        .catch(err => {
+            console.log(err);
         });
 
     // TODO: replace with results from geocoder
-    let confirmation = "The address has been successfully validated and stored into the database!"
 
-
-    console.log($('#modal').find(".modal-body"))
-    $('#modal').find(".modal-body").text(confirmation)
-    $('#modal').modal('show');
 });
 
-(function () {
+(function() {
     'use strict';
-    window.addEventListener('load', function () {
+    window.addEventListener('load', function() {
         // init bootstrap custom file upload
         bsCustomFileInput.init();
         // hide batch import tab
         document.getElementById("batchForm").style.display = "none"
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -61,209 +70,209 @@ document.getElementById("submit").addEventListener("click", () => {
 
 let stateOptionsList = document.getElementById('inputState').options;
 let stateOptions = [{
-    text: 'AL',
-    value: 'AL'
-},
-{
-    text: 'AK',
-    value: 'AK'
-},
-{
-    text: 'AZ',
-    value: 'AZ'
-},
-{
-    text: 'AZ',
-    value: 'AZ'
-},
-{
-    text: 'AR',
-    value: 'AR'
-},
-{
-    text: 'CA',
-    value: 'CA'
-},
-{
-    text: 'CO',
-    value: 'CO'
-},
-{
-    text: 'CT',
-    value: 'CT'
-},
-{
-    text: 'DE',
-    value: 'DE'
-},
-{
-    text: 'FL',
-    value: 'FL'
-},
-{
-    text: 'GA',
-    value: 'GA'
-},
-{
-    text: 'HI',
-    value: 'HI'
-},
-{
-    text: 'ID',
-    value: 'ID'
-},
-{
-    text: 'IL',
-    value: 'IL'
-},
-{
-    text: 'IN',
-    value: 'IN'
-},
-{
-    text: 'IA',
-    value: 'IA'
-},
-{
-    text: 'KS',
-    value: 'KS'
-},
-{
-    text: 'KY',
-    value: 'KY'
-},
-{
-    text: 'LA',
-    value: 'LA'
-},
-{
-    text: 'ME',
-    value: 'ME'
-},
-{
-    text: 'MD',
-    value: 'MD'
-},
-{
-    text: 'MA',
-    value: 'MA'
-},
-{
-    text: 'MI',
-    value: 'MI'
-},
-{
-    text: 'MN',
-    value: 'MN'
-},
-{
-    text: 'MS',
-    value: 'MS'
-},
-{
-    text: 'MO',
-    value: 'MO'
-},
-{
-    text: 'MT',
-    value: 'MT'
-},
-{
-    text: 'NE',
-    value: 'NE'
-},
-{
-    text: 'NV',
-    value: 'NV'
-},
-{
-    text: 'NH',
-    value: 'NH'
-},
-{
-    text: 'NJ',
-    value: 'NJ'
-},
-{
-    text: 'NM',
-    value: 'NM'
-},
-{
-    text: 'NY',
-    value: 'NY'
-},
-{
-    text: 'NC',
-    value: 'NC'
-},
-{
-    text: 'ND',
-    value: 'ND'
-},
-{
-    text: 'OH',
-    value: 'OH'
-},
-{
-    text: 'OK',
-    value: 'OK'
-},
-{
-    text: 'OR',
-    value: 'OR'
-},
-{
-    text: 'PA',
-    value: 'PA'
-},
-{
-    text: 'RI',
-    value: 'RI'
-},
-{
-    text: 'SC',
-    value: 'SC'
-},
-{
-    text: 'SD',
-    value: 'SD'
-},
-{
-    text: 'TN',
-    value: 'TN'
-},
-{
-    text: 'TX',
-    value: 'TX'
-},
-{
-    text: 'UT',
-    value: 'UT'
-},
-{
-    text: 'VT',
-    value: 'VT'
-},
-{
-    text: 'VA',
-    value: 'VA'
-},
-{
-    text: 'WA',
-    value: 'WA'
-},
-{
-    text: 'WV',
-    value: 'WV'
-},
-{
-    text: 'WI',
-    value: 'WI'
-},
-{
-    text: 'WY',
-    value: 'WY'
-},
+        text: 'AL',
+        value: 'AL'
+    },
+    {
+        text: 'AK',
+        value: 'AK'
+    },
+    {
+        text: 'AZ',
+        value: 'AZ'
+    },
+    {
+        text: 'AZ',
+        value: 'AZ'
+    },
+    {
+        text: 'AR',
+        value: 'AR'
+    },
+    {
+        text: 'CA',
+        value: 'CA'
+    },
+    {
+        text: 'CO',
+        value: 'CO'
+    },
+    {
+        text: 'CT',
+        value: 'CT'
+    },
+    {
+        text: 'DE',
+        value: 'DE'
+    },
+    {
+        text: 'FL',
+        value: 'FL'
+    },
+    {
+        text: 'GA',
+        value: 'GA'
+    },
+    {
+        text: 'HI',
+        value: 'HI'
+    },
+    {
+        text: 'ID',
+        value: 'ID'
+    },
+    {
+        text: 'IL',
+        value: 'IL'
+    },
+    {
+        text: 'IN',
+        value: 'IN'
+    },
+    {
+        text: 'IA',
+        value: 'IA'
+    },
+    {
+        text: 'KS',
+        value: 'KS'
+    },
+    {
+        text: 'KY',
+        value: 'KY'
+    },
+    {
+        text: 'LA',
+        value: 'LA'
+    },
+    {
+        text: 'ME',
+        value: 'ME'
+    },
+    {
+        text: 'MD',
+        value: 'MD'
+    },
+    {
+        text: 'MA',
+        value: 'MA'
+    },
+    {
+        text: 'MI',
+        value: 'MI'
+    },
+    {
+        text: 'MN',
+        value: 'MN'
+    },
+    {
+        text: 'MS',
+        value: 'MS'
+    },
+    {
+        text: 'MO',
+        value: 'MO'
+    },
+    {
+        text: 'MT',
+        value: 'MT'
+    },
+    {
+        text: 'NE',
+        value: 'NE'
+    },
+    {
+        text: 'NV',
+        value: 'NV'
+    },
+    {
+        text: 'NH',
+        value: 'NH'
+    },
+    {
+        text: 'NJ',
+        value: 'NJ'
+    },
+    {
+        text: 'NM',
+        value: 'NM'
+    },
+    {
+        text: 'NY',
+        value: 'NY'
+    },
+    {
+        text: 'NC',
+        value: 'NC'
+    },
+    {
+        text: 'ND',
+        value: 'ND'
+    },
+    {
+        text: 'OH',
+        value: 'OH'
+    },
+    {
+        text: 'OK',
+        value: 'OK'
+    },
+    {
+        text: 'OR',
+        value: 'OR'
+    },
+    {
+        text: 'PA',
+        value: 'PA'
+    },
+    {
+        text: 'RI',
+        value: 'RI'
+    },
+    {
+        text: 'SC',
+        value: 'SC'
+    },
+    {
+        text: 'SD',
+        value: 'SD'
+    },
+    {
+        text: 'TN',
+        value: 'TN'
+    },
+    {
+        text: 'TX',
+        value: 'TX'
+    },
+    {
+        text: 'UT',
+        value: 'UT'
+    },
+    {
+        text: 'VT',
+        value: 'VT'
+    },
+    {
+        text: 'VA',
+        value: 'VA'
+    },
+    {
+        text: 'WA',
+        value: 'WA'
+    },
+    {
+        text: 'WV',
+        value: 'WV'
+    },
+    {
+        text: 'WI',
+        value: 'WI'
+    },
+    {
+        text: 'WY',
+        value: 'WY'
+    },
 ]
 
 stateOptionsList.add(new Option('Choose...', null, true))
