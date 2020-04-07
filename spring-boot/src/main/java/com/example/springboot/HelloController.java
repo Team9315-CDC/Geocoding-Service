c1d490403c44254b52745892ddc557ea52331a51
tonyz0909 <tonyz0909@gmail.com>
2020-04-07 12:33:47 -0400

    added POST API request for batch processing

--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,16 @@
 package com.example.springboot;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.multipart.MultipartFile;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 
 @RestController
 public class HelloController {
@@ -9,6 +18,24 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@PostMapping("/test")
+	String test(@RequestParam("file") MultipartFile file) {
+		// validate file
+        if (file.isEmpty()) {
+            System.out.println("Please select a CSV file to upload.");
+        } else {
+			try {
+				System.out.println(file.getContentType());
+
+				// TODO: stream csv file to BatchCurl.java for batch processing
+				Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+			} catch (Exception e) {
+				System.out.println("exception occured: " + e.toString());
+			}
+		}
+		return "{ \"result\": \"file of type " + file.getContentType() + " received\"}";
 	}
 
 }
--- spring-boot/target/classes/static/js/scripts.js
+++ spring-boot/target/classes/static/js/scripts.js
@@ -1,7 +1,22 @@
-document.getElementById("submitBatch").addEventListener("click", () => {
-    file = document.getElementById("batchFileUpload").files;
-    console.log(file);
-    alert(file.length > 0 ? 'Uploaded file: ' + file[0].name : 'No files uploaded');
+document.getElementById("submitBatch").addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    file = document.getElementById("batchFileUpload").files[0];
+    const data = new FormData()
+    data.append('file', file)
+    console.log(data.get('file'));
+    alert(file ? 'Uploaded file: ' + file.name : 'No files uploaded');
+    fetch('http://localhost:8080/test', {
+        method: 'POST',
+        body: data,
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data.result);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
 
 document.getElementById("addressFormLink").addEventListener("click", () => {
@@ -41,15 +56,15 @@ document.getElementById("submit").addEventListener("click", () => {
         })
         .then((myJson) => {
             // setTimeout(() => {
-                spinner.setAttribute("hidden", "");
-                // console.log(myJson);
-                jsonResponse = myJson;
-                //will need to do some checks here: 
-                x = myJson.result.addressMatches[0].coordinates.x
-                y = myJson.result.addressMatches[0].coordinates.y
-                let confirmation = "The address coordinates of (lat: " + y.toString() + ", long: " + x.toString() + ") has been geocoded."
-                $('#modal').find(".modal-body").text(confirmation)
-                $('#modal').modal('show');
+            spinner.setAttribute("hidden", "");
+            // console.log(myJson);
+            jsonResponse = myJson;
+            //will need to do some checks here: 
+            x = myJson.result.addressMatches[0].coordinates.x
+            y = myJson.result.addressMatches[0].coordinates.y
+            let confirmation = "The address coordinates of (lat: " + y.toString() + ", long: " + x.toString() + ") has been geocoded."
+            $('#modal').find(".modal-body").text(confirmation)
+            $('#modal').modal('show');
             // }, 2000)
         })
         .catch(err => {
@@ -64,18 +79,18 @@ document.getElementById("submit").addEventListener("click", () => {
 
 });
 
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // init bootstrap custom file upload
         bsCustomFileInput.init();
         // hide batch import tab
         document.getElementById("batchForm").style.display = "none"
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -88,241 +103,241 @@ document.getElementById("submit").addEventListener("click", () => {
 
 let stateOptionsList = document.getElementById('inputState').options;
 let stateOptions = [{
-        "name": "Alabama",
-        "abbreviation": "AL"
-    },
-    {
-        "name": "Alaska",
-        "abbreviation": "AK"
-    },
-    {
-        "name": "American Samoa",
-        "abbreviation": "AS"
-    },
-    {
-        "name": "Arizona",
-        "abbreviation": "AZ"
-    },
-    {
-        "name": "Arkansas",
-        "abbreviation": "AR"
-    },
-    {
-        "name": "California",
-        "abbreviation": "CA"
-    },
-    {
-        "name": "Colorado",
-        "abbreviation": "CO"
-    },
-    {
-        "name": "Connecticut",
-        "abbreviation": "CT"
-    },
-    {
-        "name": "Delaware",
-        "abbreviation": "DE"
-    },
-    {
-        "name": "District Of Columbia",
-        "abbreviation": "DC"
-    },
-    {
-        "name": "Federated States Of Micronesia",
-        "abbreviation": "FM"
-    },
-    {
-        "name": "Florida",
-        "abbreviation": "FL"
-    },
-    {
-        "name": "Georgia",
-        "abbreviation": "GA"
-    },
-    {
-        "name": "Guam",
-        "abbreviation": "GU"
-    },
-    {
-        "name": "Hawaii",
-        "abbreviation": "HI"
-    },
-    {
-        "name": "Idaho",
-        "abbreviation": "ID"
-    },
-    {
-        "name": "Illinois",
-        "abbreviation": "IL"
-    },
-    {
-        "name": "Indiana",
-        "abbreviation": "IN"
-    },
-    {
-        "name": "Iowa",
-        "abbreviation": "IA"
-    },
-    {
-        "name": "Kansas",
-        "abbreviation": "KS"
-    },
-    {
-        "name": "Kentucky",
-        "abbreviation": "KY"
-    },
-    {
-        "name": "Louisiana",
-        "abbreviation": "LA"
-    },
-    {
-        "name": "Maine",
-        "abbreviation": "ME"
-    },
-    {
-        "name": "Marshall Islands",
-        "abbreviation": "MH"
-    },
-    {
-        "name": "Maryland",
-        "abbreviation": "MD"
-    },
-    {
-        "name": "Massachusetts",
-        "abbreviation": "MA"
-    },
-    {
-        "name": "Michigan",
-        "abbreviation": "MI"
-    },
-    {
-        "name": "Minnesota",
-        "abbreviation": "MN"
-    },
-    {
-        "name": "Mississippi",
-        "abbreviation": "MS"
-    },
-    {
-        "name": "Missouri",
-        "abbreviation": "MO"
-    },
-    {
-        "name": "Montana",
-        "abbreviation": "MT"
-    },
-    {
-        "name": "Nebraska",
-        "abbreviation": "NE"
-    },
-    {
-        "name": "Nevada",
-        "abbreviation": "NV"
-    },
-    {
-        "name": "New Hampshire",
-        "abbreviation": "NH"
-    },
-    {
-        "name": "New Jersey",
-        "abbreviation": "NJ"
-    },
-    {
-        "name": "New Mexico",
-        "abbreviation": "NM"
-    },
-    {
-        "name": "New York",
-        "abbreviation": "NY"
-    },
-    {
-        "name": "North Carolina",
-        "abbreviation": "NC"
-    },
-    {
-        "name": "North Dakota",
-        "abbreviation": "ND"
-    },
-    {
-        "name": "Northern Mariana Islands",
-        "abbreviation": "MP"
-    },
-    {
-        "name": "Ohio",
-        "abbreviation": "OH"
-    },
-    {
-        "name": "Oklahoma",
-        "abbreviation": "OK"
-    },
-    {
-        "name": "Oregon",
-        "abbreviation": "OR"
-    },
-    {
-        "name": "Palau",
-        "abbreviation": "PW"
-    },
-    {
-        "name": "Pennsylvania",
-        "abbreviation": "PA"
-    },
-    {
-        "name": "Puerto Rico",
-        "abbreviation": "PR"
-    },
-    {
-        "name": "Rhode Island",
-        "abbreviation": "RI"
-    },
-    {
-        "name": "South Carolina",
-        "abbreviation": "SC"
-    },
-    {
-        "name": "South Dakota",
-        "abbreviation": "SD"
-    },
-    {
-        "name": "Tennessee",
-        "abbreviation": "TN"
-    },
-    {
-        "name": "Texas",
-        "abbreviation": "TX"
-    },
-    {
-        "name": "Utah",
-        "abbreviation": "UT"
-    },
-    {
-        "name": "Vermont",
-        "abbreviation": "VT"
-    },
-    {
-        "name": "Virgin Islands",
-        "abbreviation": "VI"
-    },
-    {
-        "name": "Virginia",
-        "abbreviation": "VA"
-    },
-    {
-        "name": "Washington",
-        "abbreviation": "WA"
-    },
-    {
-        "name": "West Virginia",
-        "abbreviation": "WV"
-    },
-    {
-        "name": "Wisconsin",
-        "abbreviation": "WI"
-    },
-    {
-        "name": "Wyoming",
-        "abbreviation": "WY"
-    }
+    "name": "Alabama",
+    "abbreviation": "AL"
+},
+{
+    "name": "Alaska",
+    "abbreviation": "AK"
+},
+{
+    "name": "American Samoa",
+    "abbreviation": "AS"
+},
+{
+    "name": "Arizona",
+    "abbreviation": "AZ"
+},
+{
+    "name": "Arkansas",
+    "abbreviation": "AR"
+},
+{
+    "name": "California",
+    "abbreviation": "CA"
+},
+{
+    "name": "Colorado",
+    "abbreviation": "CO"
+},
+{
+    "name": "Connecticut",
+    "abbreviation": "CT"
+},
+{
+    "name": "Delaware",
+    "abbreviation": "DE"
+},
+{
+    "name": "District Of Columbia",
+    "abbreviation": "DC"
+},
+{
+    "name": "Federated States Of Micronesia",
+    "abbreviation": "FM"
+},
+{
+    "name": "Florida",
+    "abbreviation": "FL"
+},
+{
+    "name": "Georgia",
+    "abbreviation": "GA"
+},
+{
+    "name": "Guam",
+    "abbreviation": "GU"
+},
+{
+    "name": "Hawaii",
+    "abbreviation": "HI"
+},
+{
+    "name": "Idaho",
+    "abbreviation": "ID"
+},
+{
+    "name": "Illinois",
+    "abbreviation": "IL"
+},
+{
+    "name": "Indiana",
+    "abbreviation": "IN"
+},
+{
+    "name": "Iowa",
+    "abbreviation": "IA"
+},
+{
+    "name": "Kansas",
+    "abbreviation": "KS"
+},
+{
+    "name": "Kentucky",
+    "abbreviation": "KY"
+},
+{
+    "name": "Louisiana",
+    "abbreviation": "LA"
+},
+{
+    "name": "Maine",
+    "abbreviation": "ME"
+},
+{
+    "name": "Marshall Islands",
+    "abbreviation": "MH"
+},
+{
+    "name": "Maryland",
+    "abbreviation": "MD"
+},
+{
+    "name": "Massachusetts",
+    "abbreviation": "MA"
+},
+{
+    "name": "Michigan",
+    "abbreviation": "MI"
+},
+{
+    "name": "Minnesota",
+    "abbreviation": "MN"
+},
+{
+    "name": "Mississippi",
+    "abbreviation": "MS"
+},
+{
+    "name": "Missouri",
+    "abbreviation": "MO"
+},
+{
+    "name": "Montana",
+    "abbreviation": "MT"
+},
+{
+    "name": "Nebraska",
+    "abbreviation": "NE"
+},
+{
+    "name": "Nevada",
+    "abbreviation": "NV"
+},
+{
+    "name": "New Hampshire",
+    "abbreviation": "NH"
+},
+{
+    "name": "New Jersey",
+    "abbreviation": "NJ"
+},
+{
+    "name": "New Mexico",
+    "abbreviation": "NM"
+},
+{
+    "name": "New York",
+    "abbreviation": "NY"
+},
+{
+    "name": "North Carolina",
+    "abbreviation": "NC"
+},
+{
+    "name": "North Dakota",
+    "abbreviation": "ND"
+},
+{
+    "name": "Northern Mariana Islands",
+    "abbreviation": "MP"
+},
+{
+    "name": "Ohio",
+    "abbreviation": "OH"
+},
+{
+    "name": "Oklahoma",
+    "abbreviation": "OK"
+},
+{
+    "name": "Oregon",
+    "abbreviation": "OR"
+},
+{
+    "name": "Palau",
+    "abbreviation": "PW"
+},
+{
+    "name": "Pennsylvania",
+    "abbreviation": "PA"
+},
+{
+    "name": "Puerto Rico",
+    "abbreviation": "PR"
+},
+{
+    "name": "Rhode Island",
+    "abbreviation": "RI"
+},
+{
+    "name": "South Carolina",
+    "abbreviation": "SC"
+},
+{
+    "name": "South Dakota",
+    "abbreviation": "SD"
+},
+{
+    "name": "Tennessee",
+    "abbreviation": "TN"
+},
+{
+    "name": "Texas",
+    "abbreviation": "TX"
+},
+{
+    "name": "Utah",
+    "abbreviation": "UT"
+},
+{
+    "name": "Vermont",
+    "abbreviation": "VT"
+},
+{
+    "name": "Virgin Islands",
+    "abbreviation": "VI"
+},
+{
+    "name": "Virginia",
+    "abbreviation": "VA"
+},
+{
+    "name": "Washington",
+    "abbreviation": "WA"
+},
+{
+    "name": "West Virginia",
+    "abbreviation": "WV"
+},
+{
+    "name": "Wisconsin",
+    "abbreviation": "WI"
+},
+{
+    "name": "Wyoming",
+    "abbreviation": "WY"
+}
 ]
 
 stateOptionsList.add(new Option('Choose...', null, true))
